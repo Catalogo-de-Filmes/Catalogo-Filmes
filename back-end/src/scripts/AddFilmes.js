c03d065839fafb7b58d3e5703ac1ce9396320490
Catalogo-de-Filmes <catalogodefilmes1@gmail.com>
2025-06-10 11:04:35 -0300

    Filme 'A Culpa é das Estrelas' adicionado ao banco

--- back-end/src/scripts/AddFilmes.js
+++ back-end/src/scripts/AddFilmes.js
@@ -4,11 +4,11 @@ const prisma = new PrismaClient();
 async function main() {
   const novoFilme = await prisma.filme.create({
     data: {
-      nome: "As Branquelas",
-      descricao: "Dois irmãos agentes do FBI, Marcus e Kevin Copeland, acidentalmente evitam que bandidos sejam presos em uma apreensão de drogas. Como castigo, eles são forçados a escoltar um par de socialites nos Hamptons. Quando as meninas descobrem o plano da agência, elas se recusam a ir. Sem opções, Marcus e Kevin, dois homens negros, decidem fingir que são as irmãs e se transformam em um par de loiras.",
-      avaliacao: 5.9,
-      ondeAssistir: "Netflix", 
-      capaUrl: "https://res.cloudinary.com/dlvgggjoc/image/upload/v1747935144/20534159_h4f2ex.jpg", // URL gerada pelo cloudnary
+      nome: "A Culpa é das Estrelas",
+      descricao: "Dois jovens, Hazel e Gus, se apaixonam após se conhecerem em um grupo de apoio a pacientes com câncer. Juntos, eles enfrentam os desafios da doença e descobrem o poder do amor.",
+      avaliacao: 7.6,
+      ondeAssistir: "Diisney+", 
+      capaUrl: "https://res.cloudinary.com/dlvgggjoc/image/upload/v1747850131/91RXDK2vhzL._AC_UF1000_1000_QL80__mrs1wc.jpg", // URL gerada pelo cloudnary
       usuario: {
         connect: { id: 1 } // Deixe esse msemo
       },
@@ -17,8 +17,8 @@ async function main() {
             // ID de cada categoria: 1 - Ação; 2 - Animação; 3 - Aventura; 4 - Comédia; 5 - Documentário; 
             // 6 - Drama; 7 - Fantasia; 8 - Faroeste; 9 - Ficção Científica; 10 - Guerra; 11 - Musical;
             // 12 - Policial; 13 - Romance; 14 - Suspense; 15 - Terror;
-          { categoria: { connect: { id: 4 } } }, // Adicione quantas linhas necessáras para as categorias
-          { categoria: { connect: { id: 12 } } }  
+          { categoria: { connect: { id: 6 } } }, // Adicione quantas linhas necessáras para as categorias
+          { categoria: { connect: { id: 13 } } }  
         ]
       }
     }
