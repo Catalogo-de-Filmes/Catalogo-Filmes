a05ddffc25678bb7e90fe8794d629f5e25c41901
Catalogo-de-Filmes <catalogodefilmes1@gmail.com>
2025-06-16 11:32:22 -0300

    Update api.js

--- client/front-end/src/services/api.js
+++ client/front-end/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://catalogo-filmes-3b3k.onrender.com'
+  baseURL: 'https://catalogo-filmes-3b3k.onrender.com',
+  withCredentials: true
 });
+
 
 export default api;
