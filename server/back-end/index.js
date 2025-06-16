7db55c7b7423a5a49be5877aa209100fd7d3c9e0
Catalogo-de-Filmes <catalogodefilmes1@gmail.com>
2025-06-16 08:36:00 -0300

    Update index.js

--- server/back-end/index.js
+++ server/back-end/index.js
@@ -1,6 +1,6 @@
 const app = require('./src/app');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`✅ Servidor rodando em http://localhost:${PORT}`);
